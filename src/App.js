cef50a17e64e8e42804f26aaf8f84f34cddc6359
Brekk <brekk@brekkbockrath.com>
2020-12-31 12:52:45 +0100

    simple example for integrating bodypaint and @open-sorcerers/breakpoints

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { renderBreakpoints, renderVerticalBreakpoints } from '@open-sorcerers/breakpoints'
+import { asRem, makePainter, HORIZONTAL_BREAKPOINTS, VERTICAL_BREAKPOINTS } from 'bodypaint'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import logo from './logo.svg';
+
+export const POINTS = asRem(16, HORIZONTAL_BREAKPOINTS)
+export const VPOINTS = asRem(16, VERTICAL_BREAKPOINTS)
+
+console.log("POINTS", POINTS, "VERTICAL POINTS", VPOINTS)
+
+export const Breakpoints = renderBreakpoints(POINTS)
+export const VBreakpoints = renderVerticalBreakpoints(VPOINTS)
+
+const mq = makePainter({
+  useMin: true,
+  useHeight: false,
+  baseFontSize: 16,
+  implicit: true,
+  points: {
+    ONE: 320,
+    TWO: 480,
+    THREE: 640,
+    FOUR: 800
+  }
+})
+
+const responsiveBoxStyle = mq({
+  background: {
+    ONE: 'red',
+    TWO: 'orange',
+    THREE: 'yellow',
+    FOUR: 'green'
+  }
+})
+
+const boxStyle = css`
+  width: 5rem;
+  height: 5rem;
+  border: 10px solid black;
+  display: flex;
+  margin: auto;
+  ${responsiveBoxStyle}
+`
+console.log("BOXSTYLE", boxStyle)
+
+const App = () => (
+  <>
+    <Breakpoints />
+    <VBreakpoints />
+    <div css={boxStyle} />
+  </>
+)
 
 export default App;
